25fa16e729ed8de2e64347b85556eaec8c89028a
Bruno Coimbra <coimbra@fnal.gov>
2021-12-01 17:00:29 +0000

    Test configure_gwms_frontend   execution

--- src/decisionengine_modules/glideinwms/tests/test_configure_gwms_frontend.py
+++ src/decisionengine_modules/glideinwms/tests/test_configure_gwms_frontend.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 from decisionengine_modules.glideinwms import configure_gwms_frontend
 from decisionengine_modules.glideinwms.tests.fixtures import de_client_config  # noqa: F401
@@ -9,10 +9,9 @@ def test_get_arg_parser():
     assert isinstance(parser, ArgumentParser)
 
 
-# TODO: This test requires the new GlideinWMS RPMs to be installed.
-# def test_main():
-#     args = Namespace()
-#     args.web_base_dir = "/var/lib/gwms-frontend/web-base"
-#     args.update_scripts = False
-#     args.de_frontend_config = "/var/lib/gwms-frontend/vofrontend/de_frontend_config"
-#     configure_gwms_frontend.main(args)
+def test_main():
+    args = Namespace()
+    args.web_base_dir = "/var/lib/gwms-frontend/web-base"
+    args.update_scripts = False
+    args.de_frontend_config = "/var/lib/gwms-frontend/vofrontend/de_frontend_config"
+    configure_gwms_frontend.main(args)
